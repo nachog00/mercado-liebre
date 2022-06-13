7cef2527dfd9b4585ef2cc4d9bd273a3e5acf739
nachog00 <ignaciog00@gmail.com>
2022-06-12 23:50:01 -0300

    with this commit im testing the 'commit' script I set in the node package

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath = path.join(__dirname, 'public/')
+app.use(express.static(publicPath));
+
+const server = app.listen(1998, () => {
+    console.log("server is running")
+})
+
+
+app.get("/", function (req, res) {
+    let dir = path.resolve(__dirname, "./src/views/index.html");
+    res.sendFile(dir)
+})
